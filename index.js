65ca78f96c09ed0843c4d7bafd99c8c36db305ad
bharath sai <og@OGs-MacBook-Air.local>
2023-12-14 22:11:50 +0530

    express to create httpserver & make it receive requests on port 3000 , add gitignore

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-// function expression
-// var calculateSum = function (){
-//     var sum =0;
-//     for(int i =0;i<100;i++){
-//         sum+=i;
-//     }
-//     return sum;
-// }
+const express = require('express')
+const app = express()
+const port = 3000
 
-//function declaration
- function calculateSum(){
+function calculateSum(req, res){
     var sum =0;
     for(var i =0;i<100;i++){
         sum+=i;
     }
-    return sum;
+    // return sum;
+    res.send('Hello World! the sum is :' + sum);
 }
 
-var dispaly = calculateSum();
-console.log(dispaly);
+// when request method GET 
+//default route '/'
+app.get('/', calculateSum);
+
+
+function startListening(){
+    console.log(`Example app listening on port ${port}`)
+}
+
+//this line of code- makes this an active httpserver listening on port
+app.listen(port, startListening(port));
+
+//these few lines can run a  potential website , you can buy a domain , host it on internet.
+// add some  UI
+//do some backend process : as per usecase 
+//you have your application ready
+
